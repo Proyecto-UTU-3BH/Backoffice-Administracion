4f32862f9e58e06a02d2958421e9217f586b17bc
KRaf22 <rafagonzalezknappe@gmail.com>
2023-08-15 15:39:14 -0300

    Middleware de autentificación para todas las rutas, redirigiendo al login en caso de intentar acceder a una ruta sim haber iniciado sesion

--- routes/web.php
+++ routes/web.php
@@ -20,53 +20,60 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
 
-Route::get('/listarAlmacenes', [AlmacenController::class,'ListarAlmacenes'])->name('listarAlmacenes');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/almacenes/crearAlmacen', function () {
-    return view('crearAlmacen');
-})->name('crearAlmacen');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+    
+    Route::get('/listarAlmacenes', [AlmacenController::class,'ListarAlmacenes'])->name('listarAlmacenes');
+    
+    Route::get('/almacenes/crearAlmacen', function () {
+        return view('crearAlmacen');
+    })->name('crearAlmacen');
+    
+    Route::get('/almacenes/modificarAlmacen/{idAlmacen}', [AlmacenController::class,'ListarUnAlmacen'])->name('modificarAlmacen');
+    
+    Route::post('/almacenes/crearAlmacen', [AlmacenController::class,'InsertarAlmacen']);
+    
+    Route::post('/almacenes/modificarAlmacen/{idAlmacen}', [AlmacenController::class,'ModificarAlmacen']);
+    
+    Route::delete('/almacenes/eliminarAlmacen/{idAlmacen}', [AlmacenController::class,'EliminarAlmacen'])->name('eliminarAlmacen');
+    
+    
+    Route::get('/listarProductos', [ProductoController::class,'ListarProductos'])->name('listarProductos');
+    
+    Route::get('/productos/crearProducto', function () {
+        return view('crearProducto');
+    })->name('crearProducto');
+    
+    Route::get('/productos/modificarProducto/{idProducto}', [ProductoController::class,'ListarUnProducto'])->name('modificarProducto');
+    
+    Route::post('/productos/modificarProducto/{idProducto}', [ProductoController::class,'ModificarProducto']);
+    
+    Route::post('/productos/crearProducto', [ProductoController::class,'InsertarProducto']);
+    
+    Route::delete('/productos/eliminarProducto/{idProducto}', [ProductoController::class,'EliminarProducto'])->name('eliminarProducto');
+    
+    
+    Route::get('/listarUsuarios', [UserController::class,'ListarUsuarios'])->name('listarUsuarios');
+    
+    Route::get('/usuarios/crearUsuario', function () {
+        return view('crearUsuario');
+    })->name('crearUsuario');
+    
+    Route::get('/usuarios/modificarUsuario/{idUsuario}', [UserController::class,'ListarUnUsuario'])->name('modificarUsuario');
+    
+    Route::post('/usuarios/modificarUsuario/{idUsuario}', [UserController::class,'ModificarUsuario']);
+    
+    Route::post('/usuarios/crearUsuario', [UserController::class,'InsertarUsuario']);
+    
+    Route::delete('/usuarios/eliminarUsuario/{idUsuario}', [UserController::class,'EliminarUsuario'])->name('eliminarUsuario');
+    
+});
 
-Route::get('/almacenes/modificarAlmacen/{idAlmacen}', [AlmacenController::class,'ListarUnAlmacen'])->name('modificarAlmacen');
 
-Route::post('/almacenes/crearAlmacen', [AlmacenController::class,'InsertarAlmacen']);
-
-Route::post('/almacenes/modificarAlmacen/{idAlmacen}', [AlmacenController::class,'ModificarAlmacen']);
-
-Route::delete('/almacenes/eliminarAlmacen/{idAlmacen}', [AlmacenController::class,'EliminarAlmacen'])->name('eliminarAlmacen');
-
-
-Route::get('/listarProductos', [ProductoController::class,'ListarProductos'])->name('listarProductos');
-
-Route::get('/productos/crearProducto', function () {
-    return view('crearProducto');
-})->name('crearProducto');
-
-Route::get('/productos/modificarProducto/{idProducto}', [ProductoController::class,'ListarUnProducto'])->name('modificarProducto');
-
-Route::post('/productos/modificarProducto/{idProducto}', [ProductoController::class,'ModificarProducto']);
-
-Route::post('/productos/crearProducto', [ProductoController::class,'InsertarProducto']);
-
-Route::delete('/productos/eliminarProducto/{idProducto}', [ProductoController::class,'EliminarProducto'])->name('eliminarProducto');
-
-
-Route::get('/listarUsuarios', [UserController::class,'ListarUsuarios'])->name('listarUsuarios');
-
-Route::get('/usuarios/crearUsuario', function () {
-    return view('crearUsuario');
-})->name('crearUsuario');
-
-Route::get('/usuarios/modificarUsuario/{idUsuario}', [UserController::class,'ListarUnUsuario'])->name('modificarUsuario');
-
-Route::post('/usuarios/modificarUsuario/{idUsuario}', [UserController::class,'ModificarUsuario']);
-
-Route::post('/usuarios/crearUsuario', [UserController::class,'InsertarUsuario']);
-
-Route::delete('/usuarios/eliminarUsuario/{idUsuario}', [UserController::class,'EliminarUsuario'])->name('eliminarUsuario');
 
