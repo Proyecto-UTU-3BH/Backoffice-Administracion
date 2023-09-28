acab6a908765a49285e8400f1dc4eac1dc88feca
KRaf22 <rafagonzalezknappe@gmail.com>
2023-09-28 17:57:18 -0300

    Validacion de campos al insertar y modificar Vehiculos, devolviendo errores si los hay

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Vehiculo;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class VehiculoController extends Controller
 {
@@ -25,6 +27,22 @@ class VehiculoController extends Controller
     }
 
     public function InsertarVehiculo(Request $request){
+        $validation = Validator::make($request->all(), [
+            'matricula' => 'required|max:7|unique:vehiculos',
+            'tipo' => 'required|in:flete,reparto',
+            'capacidad' => 'required|numeric|between:0,99999.99',
+            'estado' => 'in:en almacen,en transito',
+        ], [
+            'matricula.max' => 'Máximo 7 caracteres',
+            'tipo.in' => 'El tipo debe ser "flete" o "reparto"',
+            'capacidad.between' => 'La capacidad debe estar entre 0 y 99999.99',
+            'estado.in' => 'El estado debe ser "en almacen" o "en transito"',
+        ]);
+    
+        if ($validation->fails()) {
+            return view("crearVehiculo", ["errors" => $validation->errors()]);
+        }
+    
         $vehiculo = new Vehiculo();
     
         $vehiculo->matricula = $request->post('matricula');
@@ -37,7 +55,7 @@ class VehiculoController extends Controller
         return view('crearVehiculo', [
             "mensaje" => "Vehículo creado correctamente"
         ]);
-    }    
+    }
 
     public function EliminarVehiculo(Request $request, $idVehiculo){
         $vehiculo= Vehiculo::findOrFail($idVehiculo);
@@ -48,13 +66,39 @@ class VehiculoController extends Controller
     }
 
     public function ModificarVehiculo(Request $request, $idVehiculo){
-        $vehiculo= Vehiculo::findOrFail($idVehiculo);
-
-        $vehiculo -> update($request->all());
-
-        $vehiculo -> save();
-
+        $vehiculo = Vehiculo::findOrFail($idVehiculo);
+    
+        $validation = Validator::make($request->all(), [
+            'matricula' => [
+                'required',
+                'max:7',
+                Rule::unique('vehiculos', 'matricula')->ignore($vehiculo->id),
+            ],
+            'tipo' => 'required|in:flete,reparto',
+            'capacidad' => 'required|numeric|between:0,99999.99',
+            'estado' => 'required|in:en almacen,en transito',
+        ], [
+            'matricula.required' => 'La matrícula es requerida',
+            'matricula.max' => 'Máximo 7 caracteres',
+            'matricula.unique' => 'La matrícula ya está en uso',
+            'tipo.required' => 'El tipo es requerido',
+            'tipo.in' => 'El tipo debe ser "flete" o "reparto"',
+            'capacidad.required' => 'La capacidad es requerida',
+            'capacidad.numeric' => 'La capacidad debe ser un número',
+            'capacidad.between' => 'La capacidad debe estar entre 0 y 99999.99',
+            'estado.required' => 'El estado es requerido',
+            'estado.in' => 'El estado debe ser "en almacen" o "en transito"',
+        ]);
+    
+        if ($validation->fails()) {
+            return view("modificarVehiculo", [
+                "errors" => $validation->errors(),
+                "vehiculo" => $vehiculo,
+            ]);
+        }
+    
+        $vehiculo->update($request->all());
+    
         return redirect()->route('listarVehiculos');
-
     }
 }
