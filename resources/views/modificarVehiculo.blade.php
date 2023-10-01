a3791d775afe2c26904516ba583433410cd24fe1
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-01 16:37:03 -0300

    Mostrar errores de validación al modificar vehiculo

--- resources/views/modificarVehiculo.blade.php
+++ resources/views/modificarVehiculo.blade.php
@@ -19,27 +19,50 @@
     <div id="container">
         <form action="/vehiculos/modificarVehiculo/{{$vehiculo->id}}" method="post">
             @csrf
-            <label for="matricula">Matrícula:</label>
-            <input type="text" id="matricula" name="matricula" value="{{$vehiculo->matricula}}" required><br>
+            <div class="form-group">
+                <label for="matricula">Matrícula:</label>
+                <input type="text" id="matricula" name="matricula" value="{{$vehiculo->matricula}}" required>
+                @if($errors->has('matricula'))
+                    <span class="error">
+                        @foreach($errors->get('matricula') as $error)
+                            {{ $error }}
+                            @if (!$loop->last)
+                                , 
+                            @endif
+                        @endforeach
+                    </span>
+                @endif
+            </div>
 
-            <label for="tipo">Tipo:</label>
-            <select id="tipo" name="tipo" required>
-                <option value="flete" @if($vehiculo->tipo == 'flete') selected @endif>Flete</option>
-                <option value="reparto" @if($vehiculo->tipo == 'reparto') selected @endif>Reparto</option>
-            </select>
-            
-            <br>
+            <div class="form-group">
+                <label for="tipo">Tipo:</label>
+                <select id="tipo" name="tipo" required>
+                    <option value="flete" @if($vehiculo->tipo == 'flete') selected @endif>Flete</option>
+                    <option value="reparto" @if($vehiculo->tipo == 'reparto') selected @endif>Reparto</option>
+                </select>
+                @if($errors->has('tipo'))
+                    <span class="error">{{ $errors->first('tipo') }}</span>
+                @endif
+            </div>
 
-            <label for="estado">Estado:</label>
-            <select id="estado" name="estado" required>
-                <option value="en transito" @if($vehiculo->estado == 'en transito') selected @endif>En Tránsito</option>
-                <option value="en almacen" @if($vehiculo->estado == 'en almacen') selected @endif>En Almacén</option>
-            </select>
-            
-            <br>
+            <div class="form-group">
+                <label for="estado">Estado:</label>
+                <select id="estado" name="estado" required>
+                    <option value="en transito" @if($vehiculo->estado == 'en transito') selected @endif>En Tránsito</option>
+                    <option value="en almacen" @if($vehiculo->estado == 'en almacen') selected @endif>En Almacén</option>
+                </select>
+                @if($errors->has('estado'))
+                    <span class="error">{{ $errors->first('estado') }}</span>
+                @endif
+            </div>
 
-            <label for="capacidad">Capacidad:</label>
-            <input type="number" id="capacidad" name="capacidad" step="0.01" value="{{$vehiculo->capacidad}}" required><br>
+            <div class="form-group">
+                <label for="capacidad">Capacidad:</label>
+                <input type="number" id="capacidad" name="capacidad" step="0.01" value="{{$vehiculo->capacidad}}" required>
+                @if($errors->has('capacidad'))
+                    <span class="error">{{ $errors->first('capacidad') }}</span>
+                @endif
+            </div>
 
             <input type="submit" value="Enviar">
         </form>
