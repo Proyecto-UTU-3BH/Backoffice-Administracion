e5d0e35fef1195c98f14f535e13efcd7ec76f334
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-01 16:44:48 -0300

    Validar capacidad al insertar o modificar almacen

--- resources/views/modificarAlmacen.blade.php
+++ resources/views/modificarAlmacen.blade.php
@@ -62,6 +62,9 @@
             <div class="form-group">
                 <label for="capacidad">Capacidad de Almacenamiento:</label>
                 <input type="number" id="capacidad" name="capacidad" class="remove-br" value="{{$almacen->capacidad}}" required>
+                @if($errors->has('capacidad'))
+                    <span class="error">{{ $errors->first('capacidad') }}</span>
+                @endif
             </div>
 
             <div class="form-group">
