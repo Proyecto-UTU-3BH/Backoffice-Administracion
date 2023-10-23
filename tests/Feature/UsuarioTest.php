e5457d160d59862b06e1a648d32c95867aa1c7be
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-23 14:44:14 -0300

    Se agrega pruebas de insertar usuario, eliminar existente y eliminar inexistente

--- tests/Feature/UsuarioTest.php
+++ tests/Feature/UsuarioTest.php
@@ -41,6 +41,76 @@ class UsuarioTest extends TestCase
         $user->delete();
     }
 
+    public function test_InsertarUsuario()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->post('/usuarios/crearUsuario', [
+            'usuario' => 'soychofer@yahoo.com',
+            'ci' => '41559752',
+            'primer_nombre' => 'Rafael',
+            'primer_apellido' => 'Gonzalez',
+            'segundo_apellido' => 'Knappe',
+            'calle' => 'Graciela de Gouveia',
+            'numero_de_puerta' => '602',
+            'tipo' => 'chofer',
+            'password' => 'juansito'
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('users', [
+            'usuario' => 'soychofer@yahoo.com',
+            'ci' => '41559752',
+            'primer_nombre' => 'Rafael',
+            'primer_apellido' => 'Gonzalez',
+            'segundo_apellido' => 'Knappe',
+            'calle' => 'Graciela de Gouveia',
+            'numero_de_puerta' => '602',
+            'tipo' => 'chofer',
+        ]);
+
+        $response->assertViewIs('crearUsuario');
+
+        $response->assertViewHas('mensaje', 'Usuario creado correctamente');
+
+        $user->delete();
+    }
+
+    public function test_EliminarUsuarioExistente()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this -> delete('/usuarios/eliminarUsuario/1000');
+
+        $response->assertStatus(302);
+
+       $this->assertDatabaseMissing('usuarios', [
+        'id' => '1000',
+        'deleted_at' => null
+        ]);
+
+        $response->assertRedirect(route('listarUsuarios'));
+
+        Usuario::withTrashed()->where("id",1000)->restore();
+
+        $user->delete();
+    }
+
+    public function test_EliminarUsuarioInexistente()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this -> delete('/usuarios/eliminarUsuario/321321');
+
+        $response->assertStatus(404);
+
+        $user->delete();
+    }
+
     public function test_ModificarUsuarioExistente()
     {
         $user = User::factory()->create();
